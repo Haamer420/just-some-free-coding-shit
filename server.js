791aaf26f7f0cd1b1b2ca2cd869a7976dbeac29b
Haamer420 <haamermarten925@gmail.com>
2022-10-07 18:27:30 +0300

    finished form design & server init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use("", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/form-submit", (req, res) => {
+    console.log({
+        form: req.body
+    })
+    
+    res.status(200).send("Submitted!");
+})
+
+const PORT = process.env.PORT ?? 80;
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
